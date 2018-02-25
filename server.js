4fcc42b70d261b15059ae10abb36cba3c195b0f7
vineeta27 <35964907+vineeta27@users.noreply.github.com>
2018-02-25 19:32:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-    res.send('article one requested');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/info',function(req,res) {
