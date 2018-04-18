4cccc364efad2d75fd5f74e30c42ca02b9ba7bad
vineeta27 <35964907+vineeta27@users.noreply.github.com>
2018-04-19 00:02:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ var button=document.getElementById('counter');
    var list='';
    for(var i=0; i<names.length; i++)
    {
-       list+=names[i];
+       list+='<li>'+names[i]+'</li>';
    }
    var ul=document.getElementById('ul');
    ul.innerHTML=list;
