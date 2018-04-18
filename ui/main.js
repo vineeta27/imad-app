77521abc5d27f99835fe759723ae4f3b69aac505
vineeta27 <35964907+vineeta27@users.noreply.github.com>
2018-04-18 20:16:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('Loaded');
+// change element of html
+
 var element=document.getElementById('hell');
 element.innerHTML='Hello, I am Minion';
+
+// move image
+
+var img=document.getElementById('madi');
+ img.onclick= function(){
+   img.style.marginLeft= '100px';  
+ };
