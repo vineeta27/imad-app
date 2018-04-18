a372f3cbb0dc06a4eb5adb28f4d814c5bd3becd3
vineeta27 <35964907+vineeta27@users.noreply.github.com>
2018-04-18 20:23:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,8 +9,8 @@ element.innerHTML='Hello, I am Minion';
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft+=10;
-    img.style.marginLeft=`${marginLeft} px`;
+    marginLeft=marginLeft + 10;
+    img.style.marginLeft= marginLeft + 'px';
 }
  img.onclick= function(){
    var interval=setInterval(moveRight,100);
