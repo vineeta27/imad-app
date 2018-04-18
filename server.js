dac2b1996b2925ab4d3ad9b266d94d110f821543
vineeta27 <35964907+vineeta27@users.noreply.github.com>
2018-04-18 16:53:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req,res){
+    res.send('Article one is called');
+});
+
+app.get('/article-two', function(req,res){
+    res.send('Article two is called');
+});
+app.get('/article-three', function(req,res){
+    res.send('Article three is called');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
