3e6b48f9e5ef26b5dcc8bc93b2af47aeda696af1
vineeta27 <35964907+vineeta27@users.noreply.github.com>
2018-04-18 23:35:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,20 @@
 //button
 var button=document.getElementById('counter');
-var counter=0;
  button.onclick = function(){
+      var request= new XMLHttpRequest();
+      //
+      request.onreadystatechange= function(){
+         if(request.readystate===XMLHttpRequest.DONE)
+         {
+             if(request.status===200)
+             {
+                 var counter=request.responseText;
+                 var span= document.getElementById('count');
+                 span.innerHTML=counter.toString();
+             }
+         }
+      };
      
-     
-     
-     
-     counter=counter+1;
-     var span= document.getElementById('count');
-     span.innerHTML=counter.toString();
+     request.open('GET','http://vineetasuthar2000.imad.hasura-app.io/counter',true);
+     request.send(null);
  };
