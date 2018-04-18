bfdea347b7cbe0ab02bf5fc181f89292a0ab89ef
vineeta27 <35964907+vineeta27@users.noreply.github.com>
2018-04-19 00:14:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,16 +23,27 @@ var button=document.getElementById('counter');
  var name = nameInput.value;
  var submit=document.getElementById('submit_btn')
  submit.onclick = function(){
-   
-   
-   
-   var names=['name1','name2','name3'];
-   var list='';
-   for(var i=0; i<names.length; i++)
-   {
-       list+='<li>'+names[i]+'</li>';
-   }
-   var ul=document.getElementById('ul');
-   ul.innerHTML=list;
+       var request= new XMLHttpRequest();
+          //
+          request.onreadystatechange= function(){
+             if(request.readyState===XMLHttpRequest.DONE)
+             {
+                 if(request.status===200)
+                 {
+                       var names=request.responseText;
+                       names=JSON.parse(names);
+                       var list='';
+                       for(var i=0; i<names.length; i++)
+                       {
+                           list+='<li>'+names[i]+'</li>';
+                       }
+                       var ul=document.getElementById('ul');
+                       ul.innerHTML=list;
+                 }
+             }
+          };
+         
+         request.open('GET','http://vineetasuthar2000.imad.hasura-app.io/submit-name?name='+name,true);
+         request.send(null);
      
  };
