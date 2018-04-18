d73d1c16528d37086b1d0a687a759846d4e558b1
vineeta27 <35964907+vineeta27@users.noreply.github.com>
2018-04-19 00:18:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,6 +74,14 @@ app.get('/counter', function(req,res){
    res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submit-name', function(req,res){
+   var name=req.query.name;
+   names.push(name);
+   res.send(JSON.stringify(names));
+    
+});
+
 app.get('/:name', function(req,res){
     var name=req.params.name;
     res.send(createTemplate(obj[name]));
