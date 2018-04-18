13171cc5fe9c02bcb7db9e9a1d0e606c1a1d2dc9
vineeta27 <35964907+vineeta27@users.noreply.github.com>
2018-04-18 19:44:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,8 +75,8 @@ app.get('/:name', function(req,res){
 });
 
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
