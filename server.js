cb93d8a4f23851fa0cd0d04ff613dc286b46d52c
vineeta27 <35964907+vineeta27@users.noreply.github.com>
2018-04-20 00:05:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool= require('pg').Pool;
 var crypto=require('crypto');
+var bodyParser=require('body-parser');
 
 var config={
     user: 'vineetasuthar2000',
@@ -13,6 +14,7 @@ var config={
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -57,6 +59,24 @@ app.get('/hash/:input',function(req,res){
    
    var hashedString=hash(req.params.input,'i am minion');
    res.send(hashedString);
+    
+});
+
+app.post('/create-user',function(req,res){
+     var username=req.body.username;
+     var password=req.body.password;
+     
+   var salt=crypto.getRandomBytes(128).toString('hex');
+   var dbString=hash(password,salt);
+   pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+       if(err){
+           res.status(500).send(err.toString('hex'));
+       }
+       else
+       {
+           res.send('user succesfully created' + username);
+       }
+   })
     
 });
 
