0b5302db1b744a61a0afa171f4884d916a7eedbb
vineeta27 <35964907+vineeta27@users.noreply.github.com>
2018-04-19 14:59:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ function createTemplate(data){
 }
 
 
-/*var ab=new pool(config);
+var ab=new pool(config);
 app.get('/db',function(req,res){
     ab.query('SELECT * FROM test', function(err,result){
        if(err){
@@ -87,7 +87,7 @@ app.get('/db',function(req,res){
        }
         
     });   
-});*/
+});
 
 var counter = 0;
 app.get('/counter', function(req,res){
