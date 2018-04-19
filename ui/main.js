3215f3cd7b27e814dd993c8789ad8614c68b6a3f
vineeta27 <35964907+vineeta27@users.noreply.github.com>
2018-04-20 03:28:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,4 @@
 //button
-var button=document.getElementById('counter');
- button.onclick = function(){
-      var request= new XMLHttpRequest();
-      //
-      request.onreadystatechange= function(){
-         if(request.readyState===XMLHttpRequest.DONE)
-         {
-             if(request.status===200)
-             {
-                 var counter=request.responseText;
-                 var span= document.getElementById('count');
-                 span.innerHTML=counter.toString();
-             }
-         }
-      };
-     
-     request.open('GET','http://vineetasuthar2000.imad.hasura-app.io/counter',true);
-     request.send(null);
- };
- 
- 
  var submit=document.getElementById('submit_btn')
  submit.onclick = function(){
        var request= new XMLHttpRequest();
@@ -29,22 +8,22 @@ var button=document.getElementById('counter');
              {
                  if(request.status===200)
                  {
-                       var names=request.responseText;
-                       names=JSON.parse(names);
-                       var list='';
-                       for(var i=0; i<names.length; i++)
-                       {
-                           list+='<li>'+names[i]+'</li>';
-                       }
-                       var ul=document.getElementById('ul');
-                       ul.innerHTML=list;
+                     alert('Logged in succesfully');  
+                 }
+                 else if(request.status===403){
+                     alert('Username/password incorrect');
+                 }
+                 else if(request.status===500){
+                  alert('Something went wrong on server');   
                  }
              }
           };
-          var nameInput=document.getElementById('name');
+          var username=document.getElementById('username');
+          var password=document.getElementById('pass');
           var name = nameInput.value;
          
-         request.open('GET','http://vineetasuthar2000.imad.hasura-app.io/submit-name?name='+ name,true);
-         request.send(null);
+         request.open('POST','http://vineetasuthar2000.imad.hasura-app.io/login',true);
+         request.setRequestHeader('Content-Type', 'application/json');
+         request.send(JSON.stringify({username: username, password: password}));
      
  };
