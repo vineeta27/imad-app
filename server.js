4d02945b9d94d80332fca25ff34708d281652fdd
vineeta27 <35964907+vineeta27@users.noreply.github.com>
2018-04-20 00:40:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,9 +84,9 @@ app.post('/login',function(req,res){
      var username=req.body.username;
      var password=req.body.password;
      
-   pool.query('SELECT * FROM "user" WHERE username= ($1)',[username],function(err,result){
+   pool.query('SELECT * FROM "user" WHERE username= $1',[username],function(err,result){
        if(err){
-           res.status(500).send(err.toString('hex'));
+           res.status(500).send(err.toString());
        }
        else
        {
